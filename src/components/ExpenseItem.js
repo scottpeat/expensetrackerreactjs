d2c59767d495c6f5eb88ca4aa45a5cf265c622d0
Scott Peat <scottpeatweb@gmail.com>
2022-09-26 22:00:06 +0100

    Working with data and learning props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
+        <h2>{expenseTitle}</h2>
       </div>
       <div className="expense-item__price">
-        <h2>$294.67</h2>
+        <h2>${expenseAmount}</h2>
       </div>
     </div>
   );
