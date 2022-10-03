a034e299a1bbd78af8a507f129e89d1f2ecb3751
Scott Peat <scottpeatweb@gmail.com>
2022-10-03 22:35:29 +0100

    Using Dynamic styles and implementing Bar Chart graphic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,11 +5,9 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
 
   const clickHandler = () => {
     setTitle('Updated');
-    console.log(title);
   };
 
   return (
