bf1bd2a0bdc2431511901518e27105183c47b9b9
Scott Peat <scottpeatweb@gmail.com>
2022-10-03 18:57:17 +0100

    Created ExpensesList and used conditional return statements

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,16 +13,18 @@ const ExpenseItem = (props) => {
   };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-      </div>
-      <div className="expense-item__price">
-        <h2>${props.amount}</h2>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+        </div>
+        <div className="expense-item__price">
+          <h2>${props.amount}</h2>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </li>
   );
 };
 
