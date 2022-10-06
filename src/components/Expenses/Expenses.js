3ade25011cd4b064af9637f205199441907f44ba
Scott Peat <scottpeatweb@gmail.com>
2022-10-06 08:58:01 +0100

    Working on ExpensesChart File

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import { React, useState } from 'react';
 
 const Expenses = (props) => {
